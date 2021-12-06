85c4d4d98fcc093f168be80cf25fffb72b61449e
Leanne251 <leannesmith251@gmail.com>
2021-12-06 16:57:25 +0000

    attempt at question 2

---
+++ index.js
@@ -0,0 +1,48 @@
+let myCollection = [
+    {
+      name: "School of Code mug",
+      count: 1,
+      whatILike: "It has my cute pixel character on it!"
+    },
+    {
+      name: "School of Code hat",
+      count: 2,
+      whatILike: "An often overlooked fashion accessory"
+    },
+    {
+      name: "School of Code pillow",
+      count: 1,
+      whatILike: "Eat. Sleep. Code. Repeat :)"
+    }
+  ];
+
+  console.log(myCollection);
+
+  function describeItem(item){
+    
+    if (item.count < 1){
+      console.log(`I have a ${item.name} and here's what I like about it ${item.whatILike}`)
+    }
+  }
+
+let item = {name: "School of Code mug",
+count: 1,
+whatILike: "It has my cute pixel character on it!"}
+
+console.log(describeItem(item))
+
+
+
+//   👉 2c. Create a function called describeItem, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should console.log a message according to how many of the item you have.
+
+// If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
+
+// For example, the result of calling the describeItem function with the first item in our collection would be: "I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"
+
+// Call your function below where you've defined it, handing in the first item in myCollection.
+
+// Now run the file again (using node index.js);
+
+//Create a function that tkes in a parameter... myCollection
+
+// include an if statment to check the how m
